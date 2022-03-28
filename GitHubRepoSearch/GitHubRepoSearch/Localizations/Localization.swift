ab14bbed841755dab5878ec1d7802085ace62c1d
AleCheGH <chekushkinSasha@gmail.com>
2022-03-28 16:47:07 +0200

    added elements to main ViewController

--- GitHubRepoSearch/GitHubRepoSearch/Localizations/Localization.swift
+++ GitHubRepoSearch/GitHubRepoSearch/Localizations/Localization.swift
@@ -14,4 +14,10 @@ struct Localization {
        static let search = NSLocalizedString("", comment: "search")
     }
     
+    struct Buttons {
+        static let dropDownMenu = NSLocalizedString("drop_down_menu_button", comment: "")
+        static let fullScreenSearch = NSLocalizedString("full_screen_search_button", comment: "")
+        static let clutteredScreenSearch = NSLocalizedString("cluttered_screen_search_button", comment: "")
+    }
+    
 }
--- GitHubRepoSearch/GitHubRepoSearch/ViewController.swift
+++ GitHubRepoSearch/GitHubRepoSearch/ViewController.swift
@@ -8,15 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var dropDownMenuSearch: UIButton!
+    @IBOutlet weak var fullScreenSearch: UIButton!
+    @IBOutlet weak var clutteredSpaceSearch: UIButton!
+    
+    
     let manager = RepositoryManager(matching: "Weather", sortedBy: .numberOfStars, order: .descending, perPage: 10, pageNumber: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        configureButtons()
     }
-
-
+    
+    
+    private func configureButtons() {
+        let localization = Localization.Buttons.self
+        
+        let buttons = [dropDownMenuSearch, fullScreenSearch, clutteredSpaceSearch]
+        let localizations = [localization.dropDownMenu, localization.fullScreenSearch, localization.clutteredScreenSearch]
+        
+        for index in 0..<buttons.count {
+            buttons[index]?.setTitle(localizations[index], for: .normal)
+            buttons[index]?.backgroundColor = .black
+            buttons[index]?.layer.cornerRadius = 5
+        }
+    }
+    
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        
+        switch sender {
+        case dropDownMenuSearch:
+            print("dropDownMenuSearch")
+        case fullScreenSearch:
+            print("fullScreenSearch")
+        case clutteredSpaceSearch:
+            print("clutteredSpaceSearch")
+        default:
+            return
+        }
+    }
 }
 
