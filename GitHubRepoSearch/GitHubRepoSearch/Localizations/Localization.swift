e86f2f9379be76b2f2fbf430cd20da34e6ea6c36
AleCheGH <chekushkinSasha@gmail.com>
2022-03-28 13:17:07 +0200

    added localization files

---
+++ GitHubRepoSearch/GitHubRepoSearch/Localizations/Localization.swift
@@ -0,0 +1,17 @@
+//
+//  Localization.swift
+//  GitHubRepoSearch
+//
+//  Created by Aliaksandr Chekushkin on 3/28/22.
+//
+
+import Foundation
+
+struct Localization {
+    
+    struct Label {
+       static let github = NSLocalizedString("git_hub", comment: "")
+       static let search = NSLocalizedString("", comment: "search")
+    }
+    
+}
