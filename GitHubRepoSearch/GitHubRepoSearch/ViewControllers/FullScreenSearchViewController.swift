88d52780c3a44ff20154a271b7c767e310eac29f
AleCheGH <chekushkinSasha@gmail.com>
2022-03-28 17:15:51 +0200

    added fullscreen vc

---
+++ GitHubRepoSearch/GitHubRepoSearch/ViewControllers/FullScreenSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenSearchViewController.swift
+//  GitHubRepoSearch
+//
+//  Created by Aliaksandr Chekushkin on 3/28/22.
+//
+
+import UIKit
+
+class FullScreenSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
